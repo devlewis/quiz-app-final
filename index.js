bd3fdc1583f5ed211a4efd1b4f42e1b1b58c83d2
Devree Lewis <devree.lewis@gmail.com>
2019-11-20 15:42:31 -0500

    final view

--- index.js
+++ index.js
@@ -1,66 +1,66 @@
 const STORE = [
-    {
-      question: "What is the capital of Romania?",
-      options: [
-        "Bucharest",
-        "Budapest",
-        "Sofia",
-        "Varna"
-      ],
-      correct:
-        "Bucharest",
-      country: "Romania"
-    },
-    {
-      question:
-        "What is the capital of Montenegro?",
-      options: [
-        "Timisoara",
-        "Podgorica",
-        "Dubrovnik",
-        "Budapest"
-      ],
-      correct:
-        "Podgorica",
-      country: "Montenegro"
-    },
-    {
-      question:
-        "What is the capital of North Macedonia?",
-      options: [
-        "Skopje",
-        "Tirana",
-        "Sofia",
-        "Podgorica"
-      ],
-      correct: "Skopje",
-      country: "North Macedonia"
-    },
-    {
-      question: "What is the capital of Lithuania?",
-      options: [
-        "Kaunas",
-        "Vilnius",
-        "Riga",
-        "Minsk"
-      ],
-      correct: "Vilnius",
-      country: "Lithuania"
-    },
-    {
-      question:
-        "What is the capital of Slovenia?",
-      options: [
-        "Prague",
-        "Maribor",
-        "Zagreb",
-        "Ljubljana"
-      ],
-      correct:
-        "Ljubljana",
-      country: "Slovenia"
-    }
-  ]
+  {
+    question: "What is the capital of Romania?",
+    options: [
+      "Bucharest",
+      "Budapest",
+      "Sofia",
+      "Varna"
+    ],
+    correct:
+      "Bucharest",
+    country: "Romania"
+  },
+  {
+    question:
+      "What is the capital of Montenegro?",
+    options: [
+      "Timisoara",
+      "Podgorica",
+      "Dubrovnik",
+      "Budapest"
+    ],
+    correct:
+      "Podgorica",
+    country: "Montenegro"
+  },
+  {
+    question:
+      "What is the capital of North Macedonia?",
+    options: [
+      "Skopje",
+      "Tirana",
+      "Sofia",
+      "Podgorica"
+    ],
+    correct: "Skopje",
+    country: "North Macedonia"
+  },
+  {
+    question: "What is the capital of Lithuania?",
+    options: [
+      "Kaunas",
+      "Vilnius",
+      "Riga",
+      "Minsk"
+    ],
+    correct: "Vilnius",
+    country: "Lithuania"
+  },
+  {
+    question:
+      "What is the capital of Slovenia?",
+    options: [
+      "Prague",
+      "Maribor",
+      "Zagreb",
+      "Ljubljana"
+    ],
+    correct:
+      "Ljubljana",
+    country: "Slovenia"
+  }
+]
 
 //quiz score and question number initialization
 let totalScore = 0
@@ -70,7 +70,7 @@ let incorrect = 0
 //begins the quiz
 function beginQuiz() {
   $('.views').hide()
-  $('.startView').on('click', '.start', function(event) {
+  $('.startView').on('click', '.start', function (event) {
     $('.startView').hide()
     $('.questionView').show()
     $('.questionView').append(makeQuestion())
@@ -91,33 +91,34 @@ function makeQuestion() {
 function injectHTML(i) {
   let htmlInject = $(`<form>
     <fieldset>
-    <legend>Question #${questionNum+1} of 5:</legend>
+    <legend>Question #${questionNum + 1} of 5:</legend>
     <p class="question">${STORE[i].question}</p>
     </fieldset>
   </form>`)
 
   let fieldsetFinder = $(htmlInject).find('fieldset')
 
-  STORE[i].options.forEach(function(option, index){
+  STORE[i].options.forEach(function (option, index) {
     $(`<label for="${index}">
         <input type="radio" id="${index}" value="${option}" name="option" required>
-        <span>${option}    </span><br></br>
-      </label>
+        <span>${option}</span><br></br>
+      </label></div>
       `).appendTo(fieldsetFinder)
   });
 
   $(`<button type="submit">Submit</button>
  `).appendTo(fieldsetFinder)
 
-$(`<p class="small">You've answered ${totalScore} correctly and ${incorrect} incorrectly.</p>`).appendTo(fieldsetFinder)
-  
-  return htmlInject}
+  $(`<p class="small">You've answered ${totalScore} correctly and ${incorrect} incorrectly.</p>`).appendTo(fieldsetFinder)
+
+  return htmlInject
+}
 
 //submits selected option 
 //check to see if it matches correct option in STORE
 //run response functions
 function submitOption() {
-  $('.containerBox').on('submit', function(event) {
+  $('.containerBox').on('submit', function (event) {
     event.preventDefault()
     $('.views').hide()
     $('.feedbackView').show()
@@ -125,14 +126,14 @@ function submitOption() {
     let option = selected.val()
     let correct = STORE[questionNum].correct
     if (option === correct) {
-      totalScore++;
+      totalScore++
       $('.feedbackView').html(
         `<h1 class="green">Brilliant!</h1>
           <button class="next">Next one!</button>
           <p class="centered raleway">You've answered ${totalScore} correctly out of ${STORE.length} total!</p>`
       )
     } else {
-      incorrect++;
+      incorrect++
       $('.feedbackView').html(
         `<h1 class="red">Sorry!</h1>
         <p class="question raleway centered">${STORE[questionNum].correct} is the capital of ${STORE[questionNum].country}.</p>
@@ -141,7 +142,7 @@ function submitOption() {
       )
     }
   })
-} 
+}
 
 //makes the next question
 function nextQuestion() {
@@ -159,7 +160,7 @@ function finalPage() {
 
   if (totalScore === STORE.length) {
     rspns = "Great!"
-  } else if (totalScore > (STORE.length/2)) {
+  } else if (totalScore > (STORE.length / 2)) {
     rspns = "Good job!"
   } else {
     rspns = "Try again!"
